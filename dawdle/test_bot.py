51a456be4f2670903222a37bd56cc1de90116ced
Daniel Lowe <dlowe@dlowe.net>
2021-09-14 07:28:02 -0400

    Move tests into module directory

--- dawdle/test_bot.py
+++ dawdle/test_bot.py
@@ -54,72 +54,6 @@ class TestGameDBIdleRPG(unittest.TestCase):
             db.close()
 
 
-class TestIRCMessage(unittest.TestCase):
-    def test_basic(self):
-        line = "@time=2021-07-31T13:55:00;bar=baz :nick!example@example.com PART #example :later!"
-        msg = irc.IRCClient.parse_message(None, line)
-        self.assertEqual(msg.tags, {"time": "2021-07-31T13:55:00", "bar": "baz"})
-        self.assertEqual(msg.src, "nick")
-        self.assertEqual(msg.user, "example")
-        self.assertEqual(msg.host, "example.com")
-        self.assertEqual(msg.cmd, "PART")
-        self.assertEqual(msg.args, ["#example", "later!"])
-        self.assertEqual(msg.trailing, "later!")
-        self.assertEqual(msg.line, line)
-        self.assertEqual(msg.time, 1627754100)
-
-    def test_one_trailing_arg(self):
-        line = ":foo!bar@example.com NICK :baz"
-        msg = irc.IRCClient.parse_message(None, line)
-        self.assertEqual(msg.tags, {})
-        self.assertEqual(msg.src, "foo")
-        self.assertEqual(msg.user, "bar")
-        self.assertEqual(msg.host, "example.com")
-        self.assertEqual(msg.cmd, "NICK")
-        self.assertEqual(msg.args, ["baz"])
-        self.assertEqual(msg.trailing, "baz")
-        self.assertEqual(msg.line, line)
-
-    def test_complextags(self):
-        line = "@keyone=one\\sbig\\:value;keytwo=t\\wo\\rbig\\n\\\\values :nick!example@example.com PART #example :later!"
-        msg = irc.IRCClient.parse_message(None, line)
-        self.assertEqual(msg.tags, {
-            "keyone": "one big;value",
-            "keytwo": "two\rbig\n\\values",
-            })
-
-
-    def test_notags(self):
-        line = ":nick!example@example.com PART #example :later!"
-        msg = irc.IRCClient.parse_message(None,line)
-        self.assertEqual(msg.tags, {})
-        self.assertEqual(msg.src, "nick")
-
-    def test_badtags(self):
-        line = "@asdf :nick!example@example.com PART #example :later!"
-        msg = irc.IRCClient.parse_message(None,line)
-        self.assertEqual(msg.tags, {'asdf': None})
-        self.assertEqual(msg.src, "nick")
-
-        line = "@ :nick!example@example.com PART #example :later!"
-        msg = irc.IRCClient.parse_message(None,line)
-        self.assertEqual(msg.tags, {})
-        self.assertEqual(msg.src, "nick")
-
-    def test_bad_encoding(self):
-        line = "\255\035"
-        msg = irc.IRCClient.parse_message(None, line)
-
-
-class TestIRCClient(unittest.TestCase):
-
-    def test_handle_cap(self):
-        conf._conf['botnick'] = 'foo'
-        irc = irc.IRCClient(None)
-        irc.handle_cap(irc.IRCClient.Message(tags={}, src='tungsten.libera.chat', user=None, host=None, cmd='CAP', args=['*', 'ACK', 'multi-prefix'], trailing='multi-prefix', line=':tungsten.libera.chat CAP * ACK :multi-prefix', time=1629501206))
-        self.assertIn("multi-prefix", irc._caps)
-
-
 class FakeIRCClient(object):
     def __init__(self):
         self._nick = 'dawdlerpg'
@@ -165,18 +99,6 @@ class FakeGameStorage(bot.GameStorage):
 
     def delete(self, pname):
         self._mem.pop(pname)
-
-
-class TestIRCClient(unittest.TestCase):
-
-    def test_nick_change(self):
-        conf._conf['botnick'] = 'dawdlerpg'
-        testbot = bot.DawdleBot(bot.GameDB(FakeGameStorage()))
-        testirc = irc.IRCClient(testbot)
-        testirc.handle_join(irc.IRCClient.Message(tags={}, src='foo', user=None, host=None, cmd='NICK', args=['#dawdlerpg'], trailing='', line='', time=0))
-        testirc.handle_nick(irc.IRCClient.Message(tags={}, src='foo', user=None, host=None, cmd='NICK', args=['bar'], trailing='bar', line='', time=0))
-        self.assertNotIn('foo', testirc._users)
-        self.assertIn('bar', testirc._users)
 
 
 class TestBot(unittest.TestCase):
@@ -532,7 +454,7 @@ class TestQuest(unittest.TestCase):
 
     def setUp(self):
         conf._conf['rpbase'] = 600
-        conf._conf['datadir'] = os.path.join(os.path.dirname(__file__), "data")
+        conf._conf['datadir'] = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
         conf._conf['eventsfile'] = "events.txt"
         conf._conf['writequestfile'] = True
         conf._conf['questfilename'] = "/tmp/testquestfile.txt"
@@ -722,7 +644,7 @@ class TestGameTick(unittest.TestCase):
         conf._conf['rpstep'] = 1.14
         conf._conf['detectsplits'] = True
         conf._conf['splitwait'] = 300
-        conf._conf['datadir'] = os.path.join(os.path.dirname(__file__), "data")
+        conf._conf['datadir'] = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
         conf._conf['eventsfile'] = "events.txt"
         conf._conf['writequestfile'] = True
         conf._conf['questfilename'] = "/tmp/testquestfile.txt"
