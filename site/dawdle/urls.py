f8e9604b5482207e56f819041d489fe7ad93991c
Daniel Lowe <dlowe@dlowe.net>
2021-09-18 08:14:36 -0400

    Added player map rendering for list and detail

--- site/dawdle/urls.py
+++ site/dawdle/urls.py
@@ -7,6 +7,8 @@ app_name = 'dawdle'
 urlpatterns = [
     path('', views.FrontView.as_view(), name='front'),
     path('about', views.AboutView.as_view(), name='about'),
+    path('map', views.MapView.as_view(), name='map'),
+    path('map/<slug:player>', views.MapView.as_view(), name='map'),
     path('players', views.PlayerListView.as_view(), name='player-list'),
     path('player/<slug:pk>', views.PlayerDetailView.as_view(), name='player-detail'),
 ]
--- site/dawdle/views.py
+++ site/dawdle/views.py
@@ -1,13 +1,44 @@
-from django.shortcuts import render
-from .models import Player, Item
+import io
+import os
+
 from django.db.models import Sum
-from django.views import generic
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import generic, View
+
+from PIL import Image, ImageDraw
+
+from .models import Player, Item
+from project.settings import BASE_DIR
 
 class FrontView(generic.TemplateView):
     template_name = "dawdle/front.html"
 
+
 class AboutView(generic.TemplateView):
     template_name = "dawdle/about.html"
+
+
+class MapView(View):
+
+    def get(self, request, *args, **kwargs):
+        base_map = Image.open(os.path.join(BASE_DIR, "dawdle/static/dawdle/map.png"))
+        full_map = base_map.copy()
+        draw = ImageDraw.Draw(full_map)
+        if 'player' in kwargs:
+            pquery = Player.objects.filter(name=kwargs['player'])
+            dotsize = 3
+        else:
+            pquery = Player.objects
+            dotsize = 1
+
+        for p in pquery.all():
+            color = (0xff, 0x00, 0x00) if p.online else (0xee, 0xee, 0xee)
+            draw.ellipse([p.posx-dotsize, p.posy-dotsize, p.posx+dotsize, p.posy+dotsize], outline=color, fill=color)
+        map_bytes = io.BytesIO()
+        full_map.save(map_bytes, format="png")
+        return HttpResponse(map_bytes.getvalue(), content_type='image/png')
+
 
 class PlayerListView(generic.ListView):
     model = Player
