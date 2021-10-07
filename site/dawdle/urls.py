2e19967a05e081e4b6a93e12d1e4b99e7c6fb64a
Daniel Lowe <dlowe@dlowe.net>
2021-10-07 07:30:30 -0400

    Match player names with path and not slugs

--- site/dawdle/urls.py
+++ site/dawdle/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('', views.FrontView.as_view(), name='front'),
     path('about', views.AboutView.as_view(), name='about'),
     path('map', views.MapView.as_view(), name='map'),
-    path('map/<slug:player>', views.MapView.as_view(), name='player-map'),
+    path('map/<path:player>', views.MapView.as_view(), name='player-map'),
     path('map/quest', views.MapView.as_view(), {'quest': 'quest'}, name='quest-map'),
     path('players', views.PlayerListView.as_view(), name='player-list'),
-    path('player/<slug:pk>', views.PlayerDetailView.as_view(), name='player-detail'),
+    path('player/<path:pk>', views.PlayerDetailView.as_view(), name='player-detail'),
     path('quest', views.QuestView.as_view(), name='quest'),
 ]
