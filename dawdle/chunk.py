975d7c95c9ed4eef755a0667b3bb4ed3873cd88a
Daniel Lowe <dlowe@dlowe.net>
2021-10-25 07:00:38 -0400

    Improve chunking implementation

--- dawdle/chunk.py
+++ dawdle/chunk.py
@@ -1,29 +1,22 @@
-#!/usr/bin/python3
-
 import itertools
-import sys
 from typing import Iterable, Iterator, Generator, Tuple, TypeVar
 
+
 T = TypeVar("T")
+
 
 def chunk(iterable: Iterable[T], n: int) -> Iterator[Tuple[T, ...]]:
     """Collect data into chunks of size not more than n.
 
-    The la
+    The last chunk may be a different length than n.
     chunk('ABCDEFG', 3) --> ABC DEF G
     """
     iterator = iter(iterable)
-    group = list()
     while True:
-        try:
-            for _ in range(0,n):
-                group.append(next(iterator))
-            yield tuple(group)
-            group.clear()
-        except StopIteration:
-            if group:
-                yield tuple(group)
+        group = tuple(itertools.islice(iterator, 0, n))
+        if not group:
             break
+        yield group
 
 
 def padded_chunk(iterable: Iterable[T], n: int, pad: T) -> Iterator[Tuple[T, ...]]:
@@ -33,15 +26,9 @@ def padded_chunk(iterable: Iterable[T], n: int, pad: T) -> Iterator[Tuple[T, ...
     padded_chunk('ABCDEFG', 3, 'x') --> ABC DEF Gxx
     """
     iterator = iter(iterable)
-    group = list()
     while True:
-        try:
-            for _ in range(0,n):
-                group.append(next(iterator))
-            yield tuple(group)
-            group.clear()
-        except StopIteration:
-            if group:
-                group.extend(itertools.repeat(pad, n - len(group)))
-                yield tuple(group)
+        group = list(itertools.islice(iterator, 0, n))
+        if not group:
             break
+        group.extend(itertools.repeat(pad, n - len(group)))
+        yield tuple(group)
